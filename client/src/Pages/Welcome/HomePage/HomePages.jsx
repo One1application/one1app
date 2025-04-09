2867435cc59eb75556c78792b997492eff1b546f
piyush-github12 <piyushgithubaccouunt@gmail.com>
2025-04-09 17:26:58 +0530

    user image again

--- client/src/Pages/Welcome/HomePage/HomePages.jsx
+++ client/src/Pages/Welcome/HomePage/HomePages.jsx
@@ -4,7 +4,7 @@ import Navbar from "../../../components/NavBar/NavBar";
 import Footer from "../../../components/Footer/Footer";
 import personImage from "../../../assets/Student.png";
 import AboutImg from "../../../assets/aboutImg.png";
-import TestimonialImg from "../../../assets/User.png";
+import TestimonialImg from "../../../assets/user.png";
 import FeatureImg from "../../../assets/effortless monetizw.webp";
 import FeatureImg2 from "../../../assets/Bost Engagement.webp";
 import FeatureImg3 from "../../../assets/Flexible scheduling.jpg";
