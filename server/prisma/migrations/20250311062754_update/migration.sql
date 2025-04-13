9c811938bb75c3e9b91483380f3b2295142c83cb
vishal tiwari <vikranta1931@gmail.com>
2025-04-13 11:42:11 +0530

    updated

--- server/prisma/migrations/20250311062754_update/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "TelegramSubscription" ADD COLUMN     "chatId" TEXT;
--- server/prisma/migrations/20250311072510_new/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[boughtById]` on the table `TelegramSubscription` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "TelegramSubscription_boughtById_key" ON "TelegramSubscription"("boughtById");
